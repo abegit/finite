f4dbf1c361d7f52757a1325aaf06e5af66d25918
abraham <abe@unscene.us>
2014-11-13 16:43:26 -0800

    no message

--- header-shop.php
+++ header-shop.php
@@ -23,8 +23,7 @@
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
     <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css?=v6">
      <style>
-    #header.wrapper {background-image:url('<?php header_image(); ?>') !important;}
-    #logo {background-image:url('<?php bloginfo('template_directory'); ?>/images/logo.png') !important;}
+    #logo {background-image:url('<?php header_image(); ?>') !important;}
     </style>
 <!--Google Analytics-->
 <meta name="google-site-verification" content="3C5HGnbFsnL5Asr2pr3GVBmIrIjtawd8cXJ6aMwVNlg" />
