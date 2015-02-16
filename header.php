565b72e079c4054be68a1ba9bf234c9d9f0826ad
abraham <abe@unscene.us>
2015-02-16 10:15:25 -0800

    no message

--- header.php
+++ header.php
@@ -25,20 +25,7 @@
     <style>
     #logo {background-image:url('<?php header_image(); ?>') !important;}
     </style>
-<!--Google Analytics-->
-<meta name="google-site-verification" content="3C5HGnbFsnL5Asr2pr3GVBmIrIjtawd8cXJ6aMwVNlg" />
-  <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-28070375-1', 'unscene.us');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
-
-</script>
-<meta name="google-site-verification" content="OY-BXpM5ACDxpGSa7pqhvdMsHqIyprZui6HCuCeZBoU">
+  <?php get_template_part('analyticstracking') ?>
 <?php wp_head(); ?>
 <!--contact tab-->
 <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/fix.css?=v1">
